f74a41309aa5cf87164c8724fc224ce28ccc90df
Mike Lee <mikelee2082@gmail.com>
2019-05-29 04:38:34 +0000

    Storing allowed relationship types

--- janusgraph/models.py
+++ janusgraph/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class RelationshipTypeModel(models.Model):
+    REL_TYPES = [
+            ('parent', 'parent'),
+            ('child', 'child'),
+            ('married', 'married'),
+            ('sibling', 'sibling'),
+            ('employer', 'employer'),
+            ('employee', 'employee'),
+            ('associate', 'associate')
+    ]
+    name = models.CharField(choices=REL_TYPES, max_length=50)
+    reverse = models.CharField(choices=REL_TYPES, max_length=50)
+
+    def __str__(self):
+        return self.name
